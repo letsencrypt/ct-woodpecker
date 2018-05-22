d6ea08f5fd90efb490b32f150a8d8b5036fdef0c
Daniel McCarney <daniel@binaryparadox.net>
2018-05-22 16:05:39 -0400

    Add daemon to monitor log STH freshness. (#6)
    This PR adds an initial `ct-woodpecker` command. Once invoked this command starts a daemon process that offers a HTTP server with a Prometheus `/metrics` endpoint. The daemon remains resident after startup to monitor configured certificate transparency logs, publishing observations as metrics data.
    
    Each monitored log will have its STH fetched periodically. The latency of the fetch operation is published as a Prometheus histogram observation. Any errors in fetching the STH will increment a Prometheus counter. If the STH fetch succeeds and the signature validates successfully then the STH timestamp is published as a Prometheus gauge observation. The time since the STH timestamp is also published as a Prometheus gauge observation. This allows writing an alert to ensure that the monitored STH timestamps are always increasing as expected. 
    
    This is *not* sufficient for a full blown CT monitor but it's a start :-) Resolves https://github.com/letsencrypt/ct-woodpecker/issues/1

---
+++ cmd/ct-woodpecker/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
+	"time"
+
+	"github.com/jmhodges/clock"
+	"github.com/letsencrypt/ct-woodpecker/monitor"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	clk    clock.Clock = clock.Default()
+	logger *log.Logger = log.New(
+		os.Stdout,
+		path.Base(os.Args[0])+" ",
+		log.LstdFlags)
+)
+
+// config is a struct holding the command line configuration data
+type config struct {
+	STHFetchInterval string
+	MetricsAddr      string
+	Logs             []logConfig
+}
+
+// logConfig describes a log to be monitored
+type logConfig struct {
+	URI string
+	Key string
+}
+
+// Valid checks that a logConfig is valid. If the log has no URI, an invalid
+// URI, or no Key configured then an error is returned.
+func (lc logConfig) Valid() error {
+	if lc.URI == "" {
+		return errors.New("log URI must not be empty")
+	}
+	if url, err := url.Parse(lc.URI); err != nil {
+		return fmt.Errorf("log URI %q is invalid: %s", lc.URI, err.Error())
+	} else if url.Scheme != "http" && url.Scheme != "https" {
+		return fmt.Errorf("log URI %q is invalid: protocol scheme must be http:// or https://", lc.URI)
+	}
+	if lc.Key == "" {
+		return errors.New("log Key must not be empty")
+	}
+	return nil
+}
+
+// Valid checks that a config is valid. If the STHFetchInterval is invalid,
+// or there are no logs configured, or a configured log is invalid, then an
+// error is returned. If no MetricsAddr is provided the default will be
+// populated.
+func (c *config) Valid() error {
+	if _, err := time.ParseDuration(c.STHFetchInterval); err != nil {
+		return err
+	}
+	if c.MetricsAddr == "" {
+		c.MetricsAddr = ":1971"
+	}
+	if len(c.Logs) < 1 {
+		return errors.New("At least one log must be configured")
+	}
+	for _, lc := range c.Logs {
+		if err := lc.Valid(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Load unmarshals the JSON contents stored in the file path provided,
+// populating the configuration object. An error is returned if the populated
+// configuration is not valid.
+func (c *config) Load(file string) error {
+	if file == "" {
+		return errors.New("Config file path must not be empty")
+	}
+
+	configBytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(configBytes, c)
+	if err != nil {
+		return err
+	}
+
+	return c.Valid()
+}
+
+// waitForSignal blocks forever waiting for SIGTERM, SIGINT or SIGHUP to arrive
+// from the OS. When one of these signals occurs the provided callback is run
+// and the program exits.
+func waitForSignal(callback func()) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	signal.Notify(sigChan, syscall.SIGINT)
+	signal.Notify(sigChan, syscall.SIGHUP)
+
+	// Block waiting for a signal to arrive
+	sig := <-sigChan
+	logger.Printf("Caught %s signal\n", sig.String())
+	callback()
+	logger.Printf("Goodbye\n")
+	os.Exit(0)
+}
+
+// initMetrics creates a HTTP server listening on the provided addr with
+// a Prometheus handler registered for the /metrics URL path. The server is
+// started on a dedicated goroutine before returning to the caller.
+func initMetrics(addr string) *http.Server {
+	// Create an HTTP server for Prometheus metrics to be served from.
+	statsServer := &http.Server{
+		Addr: addr,
+	}
+	http.Handle("/metrics", promhttp.Handler())
+	logger.Printf("Handling /metrics on %s\n", addr)
+	// Run the HTTP server in its own goroutine
+	go func() {
+		err := statsServer.ListenAndServe()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}()
+	return statsServer
+}
+
+func main() {
+	configFile := flag.String(
+		"config",
+		"test/config.json",
+		"JSON ct-woodpekcer configuration file path")
+	flag.Parse()
+
+	// Load and validate the configuration from the provided JSON
+	var conf config
+	if err := conf.Load(*configFile); err != nil {
+		log.Fatalf("Unable to load ct-woodpecker config: %s", err)
+	}
+
+	// Set up the Prometheus metrics HTTP server
+	statsServer := initMetrics(conf.MetricsAddr)
+
+	// Create and start monitors to do the work of monitoring the provided log
+	fetchInterval, err := time.ParseDuration(conf.STHFetchInterval)
+	if err != nil {
+		log.Fatalf("Configured STH Fetch Interval is invalid: %s", err.Error())
+	}
+	for _, logConf := range conf.Logs {
+		m, err := monitor.New(
+			logConf.URI,
+			logConf.Key,
+			fetchInterval,
+			logger,
+			clk)
+		if err != nil {
+			log.Fatalf("Unable to create monitor for log %q: %s",
+				logConf.URI, err.Error())
+		}
+		m.Run()
+	}
+
+	// Block the main goroutine waiting for signals while the monitors run in
+	// their own goroutines. waitForSignal is provided a callback to cleanly
+	// shutdown the metrics HTTP server when a signal is caught.
+	waitForSignal(func() {
+		err := statsServer.Shutdown(context.Background())
+		if err != nil {
+			logger.Printf("Unable to shutdown statsServer cleanly: %s\n",
+				err.Error())
+		}
+	})
+}
---
+++ monitor/monitor.go
@@ -0,0 +1,150 @@
+package monitor
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	ctClient "github.com/google/certificate-transparency-go/client"
+	"github.com/google/certificate-transparency-go/jsonclient"
+	"github.com/jmhodges/clock"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+// monitorStat is a struct collecting up various prometheus metrics a monitor
+// will export/track.
+type monitorStats struct {
+	sthTimestamp *prometheus.GaugeVec
+	sthAge       *prometheus.GaugeVec
+	sthFailures  *prometheus.CounterVec
+	sthLatency   *prometheus.HistogramVec
+}
+
+const (
+	// sthTimeout controls how long should each STH fetch wait before timing out
+	sthTimeout = time.Second * 15
+)
+
+var (
+	// internetFacingBuckets are histogram buckets suitable for measuring
+	// latencies that involve traversing the public internet.
+	internetFacingBuckets               = []float64{.1, .25, .5, 1, 2.5, 5, 7.5, 10, 15, 30, 45}
+	stats                 *monitorStats = &monitorStats{
+		sthTimestamp: promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "sth_timestamp",
+			Help: "Timestamp of observed CT log signed tree head (STH)",
+		}, []string{"uri"}),
+		sthAge: promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "sth_age",
+			Help: "Elapsed time since observed CT log signed tree head (STH) timestamp",
+		}, []string{"uri"}),
+		sthFailures: promauto.NewCounterVec(prometheus.CounterOpts{
+			Name: "sth_failures",
+			Help: "Count of failures fetching CT log signed tree head (STH)",
+		}, []string{"uri"}),
+		sthLatency: promauto.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    "sth_latency",
+			Help:    "Latency observing CT log signed tree head (STH)",
+			Buckets: internetFacingBuckets,
+		}, []string{"uri"}),
+	}
+)
+
+// Monitor is a struct for monitoring a CT log.
+type Monitor struct {
+	logger *log.Logger
+	clk    clock.Clock
+	stats  *monitorStats
+	logURI string
+	logKey string
+	client *ctClient.LogClient
+	// How long to sleep between fetching the log's current STH
+	sthFetchInterval time.Duration
+}
+
+// New creates a Monitor for the given parameters. The b64key parameter is
+// expected to contain the PEM encoded public key used to verify the log's STH
+// _without_ the PEM header/footer.
+func New(
+	uri, b64key string,
+	sthFetchInterval time.Duration,
+	logger *log.Logger,
+	clk clock.Clock) (*Monitor, error) {
+	hc := &http.Client{
+		Timeout: time.Minute,
+	}
+
+	// By convention CT log public keys are shared/configured in base64 encoded
+	// DER. The `ctclient.New()` constructor expects a vanilla PEM block, that is,
+	// base64 encoded DER surronded by a header/footer. We manufacture such
+	// a block here using the b64key
+	pubkey := fmt.Sprintf("-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----", b64key)
+
+	// Create a CT client for the log. We pass a PublicKey in the
+	// `jsonclient.Options` to ensure that the STH signature will be validated
+	// when we call the client's `GetSTH` function. If this parameter is nil no
+	// signature check is performed.
+	client, err := ctClient.New(uri, hc, jsonclient.Options{
+		Logger:    logger,
+		PublicKey: pubkey,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Monitor{
+		logger:           logger,
+		clk:              clk,
+		stats:            stats,
+		logURI:           uri,
+		logKey:           pubkey,
+		client:           client,
+		sthFetchInterval: sthFetchInterval,
+	}, nil
+}
+
+// observeSTH fetches the monitored log's signed tree head (STH). The latency of
+// this operation is published to the `sthLatency` metric. The clocktime elapsed
+// since the STH's timestamp is published to the `sthAge` metric. If an error
+// occurs the `sthFailures` metric will be incremented. If the operation
+// succeeds then the `sthTimestamp` gauge will be updated to the returned STH's
+// timestamp.
+func (m *Monitor) observeSTH() {
+	labels := prometheus.Labels{"uri": m.logURI}
+	m.logger.Printf("Fetching STH for %q\n", m.logURI)
+
+	start := m.clk.Now()
+	ctx, cancel := context.WithTimeout(context.Background(), sthTimeout)
+	defer cancel()
+	sth, err := m.client.GetSTH(ctx)
+	elapsed := m.clk.Since(start)
+	m.stats.sthLatency.With(labels).Observe(elapsed.Seconds())
+
+	if err != nil {
+		m.logger.Printf("!! Error fetching STH: %#v\n", err)
+		m.stats.sthFailures.With(labels).Inc()
+		return
+	}
+
+	m.stats.sthTimestamp.With(labels).Set(float64(sth.Timestamp))
+	ts := time.Unix(0, int64(sth.Timestamp)*int64(time.Millisecond))
+	sthAge := m.clk.Since(ts)
+	m.stats.sthAge.With(labels).Set(sthAge.Seconds())
+
+	m.logger.Printf("STH for %q verified. Timestamp: %s Age: %s\n", m.logURI, ts, sthAge)
+}
+
+// Run starts the log monitoring process by creating a goroutine that will loop
+// forever fetching the log's STH and then sleeping.
+func (m *Monitor) Run() {
+	go func() {
+		for {
+			m.observeSTH()
+			m.logger.Printf("Sleeping for %s\n", m.sthFetchInterval)
+			time.Sleep(m.sthFetchInterval)
+		}
+	}()
+}
