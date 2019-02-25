60bfe10b4da83ff86db2a597d7212e0095accf4e
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2019-02-25 12:39:47 -0800

    Remove FetchBatchSize and update other comments. (#86)
    FetchBatchSize didn't really need to be configurable, since we can
    always request a reasonably large size, and logs will give us back the
    maximum that they will allow up to that size. Having fewer config options
    makes the tool easier to use.
    
    This also adds godoc documenting some config options.

--- monitor/inclusion_checker_test.go
+++ monitor/inclusion_checker_test.go
@@ -55,9 +55,7 @@ func TestGetEntries(t *testing.T) {
 			stdout: log.New(os.Stdout, "", log.LstdFlags),
 			stderr: log.New(os.Stdout, "", log.LstdFlags),
 		},
-		&InclusionOptions{
-			FetchBatchSize: 0,
-		},
+		&InclusionOptions{},
 		mc,
 		logKey,
 		mdb)
@@ -73,7 +71,10 @@ func TestGetEntries(t *testing.T) {
 	// return three entries, one at a time (because batchSize is 0)
 	mc.GetEntriesFunc = func(_ context.Context, start, end int64) ([]ct.LogEntry, error) {
 		entries := []ct.LogEntry{{}, {}, {}}
-		return entries[start : end+1], nil
+		if int(end) > len(entries) {
+			end = int64(len(entries)) - 1
+		}
+		return entries[start:end], nil
 	}
 	ic.client = mc
 	newHead, entries, err := ic.getEntries(0, 3)
@@ -339,9 +340,7 @@ func TestCheckInclusion(t *testing.T) {
 			stdout: log.New(os.Stdout, "", log.LstdFlags),
 			stderr: log.New(os.Stdout, "", log.LstdFlags),
 		},
-		&InclusionOptions{
-			FetchBatchSize: 1000,
-		},
+		&InclusionOptions{},
 		mc,
 		logKey,
 		mdb)
